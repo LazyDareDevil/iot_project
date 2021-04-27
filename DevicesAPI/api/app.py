422a97d530eb58bef600ef210fd4ecb923a8ffff
LazyDareDevil <lazy.dare.devil@gmail.com>
2021-04-27 19:06:38 +0300

    testing

--- DevicesAPI/api/app.py
+++ DevicesAPI/api/app.py
@@ -1,36 +1,11 @@
 from flask import Flask, abort, request, jsonify
-from Devices.system import SystemInfo
-from Devices.status import SystemShapshot
 
 app = Flask(__name__)
-system_data = SystemInfo()
-system_snapshot = SystemShapshot()
 
 
-@app.route("/api/v1.0/system/device", methods=['POST'])
+@app.route("/api/v1.0/test", methods=['GET'])
 def add_device():
-    if (not request.get_json(force=True)) or (not ('uid' in request.json)):
-        abort(400)
-    res = system_data.add_device(request.json['uid'])
-    device = {"uid": request.json['uid'], "status": res}
-    system_snapshot.update_info()
-    return jsonify({'device': device}), 201
-
-
-@app.route("/api/v1.0/info/device", methods=['POST'])
-def update_info():
-    if (not request.get_json(force=True)) or (not ('uid' in request.json)):
-        abort(400)
-    uid = request.json["uid"]
-    try:
-        for element in system_snapshot.devices:
-            if element.uid == uid:
-                element.change(1, request.json["light"], request.json["lighter"], request.json["motor"])
-    except Exception:
-        pass
-    device = {"uid": request.json["uid"], "change": 1, "lighter": "100 100 100", "motor": -1}
-    return jsonify({'device': device}), 201
-
+    return jsonify({'device': "rpi"}), 201
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port="3096")
